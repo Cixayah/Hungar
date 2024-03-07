4ce88d588395d07067c9bc26da83e13c860de1f5
Cix <gabrielviniciodacosta@gmail.com>
2024-03-07 16:22:06 -0300

    #

--- cogs/cog_model/cog_structure.py
+++ cogs/cog_model/cog_structure.py
@@ -8,6 +8,6 @@ class Messages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         super().__init__()
-        
+#    @app_commands.command()  #precisa estar dentro da classe 
 async def setup(bot):
     await bot.add_cog()
